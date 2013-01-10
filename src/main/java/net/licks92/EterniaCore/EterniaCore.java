119c5e7acc376c46d083508d2f6f925ad19e9ab3
Licks92 <licks92@gmail.com>
2013-01-10 23:05:48 +0100

    Create main plugin class

---
+++ src/main/java/net/licks92/EterniaCore/EterniaCore.java
@@ -0,0 +1,19 @@
+package net.licks92.EterniaCore;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EterniaCore extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
+	}
+}
